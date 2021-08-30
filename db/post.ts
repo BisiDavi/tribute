333462cc5396c84225575f837e27792b8d97c810
BisiDavi <oludavidconnect@gmail.com>
2021-08-30 15:45:16 +0100

    post page added

--- db/post.ts
+++ db/post.ts
@@ -1,1 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema(
+  {
+    fullName: String,
+    post: String,
+    email: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.Posts || mongoose.model("Posts", PostSchema);
